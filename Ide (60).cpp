68617672fb55fff6742dae6fdbe9af2202554884
Harsh26agarwal <96969803+Harsh26agarwal@users.noreply.github.com>
2022-01-04 21:39:10 +0530

    Update Ide (60).cpp

--- Ide (60).cpp
+++ Ide (60).cpp
@@ -3,21 +3,20 @@ using namespace std;
 bool canPairs(int a[], int n, int k)  
 {
 	if (n%2)
-		return false;
+        return false;
 	unordered_map<int, int> m;
 	for (int i = 0; i < n; i++)
-		m[a[i] % k]++;
+	m[a[i] % k]++;
 	for (int i = 0; i < n; i++) {
-		int rem = a[i] % k;
-		if (2 * rem == k) {
-			if (m[rem] % 2!= 0)
-				return false;
-		}
-		else if (rem == 0) {
-			if (m[rem]%2==0)
-				return false;}
-		else if (m[rem] !=m[k - rem])
-			return false;
+	int rem = a[i] % k;
+	if (2 * rem == k) {
+	if (m[rem] % 2!= 0)
+	return false;}	
+	else if (rem == 0) {
+	if (m[rem]%2!=0)
+	return false;}
+	else if (m[rem]!=m[k - rem])
+	return false;
 	}
-      return true;
+        return true;
 }
