c2c381676c3fc9e8ceefdba9619b36b2e4d266be
Harsh26agarwal <96969803+Harsh26agarwal@users.noreply.github.com>
2022-01-05 16:25:32 +0530

    Add files via upload

---
+++ Ide (63).cpp
@@ -0,0 +1,36 @@
+class Solution{
+public:
+    string decodedString(string s){
+         stack<char> st;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ']') {
+            string temp;
+            while (!st.empty() && st.top() != '[') {
+                temp = st.top() + temp;
+                st.pop();
+            }
+            st.pop();
+            string num;
+            while (!st.empty() && isdigit(st.top())) {
+                num = st.top() + num;
+                st.pop();
+            }
+            int number = stoi(num);
+            string repeat;
+            for (int j = 0; j < number; j++)
+                repeat += temp;
+            for (char c : repeat)
+                st.push(c);
+        }
+      
+        else
+            st.push(s[i]);
+    }
+    string res;
+    while (!st.empty()) {
+        res = st.top() + res;
+        st.pop();
+    }
+    return res;
+}
+};
